94b060d8aad6919dd7edc5aa4b0f4d77b87ef39d
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-04-23 14:47:09 -0400

    Update h2_ucc1_expgen_vqe.cpp

--- examples/h2_ucc1_expgen_vqe.cpp
+++ examples/h2_ucc1_expgen_vqe.cpp
@@ -9,13 +9,13 @@ int main(int argc, char **argv) {
 
   auto geom = R"geom(2
 
-H          0.00000        0.00000        0.00000
-H          0.00000        0.00000        0.7474)geom";
+    H          0.00000        0.00000        0.00000
+    H          0.00000        0.00000        0.7474)geom";
 
   auto op = qcor::getObservable("chemistry",
                                 {{"basis", "sto-3g"}, {"geometry", geom}});
 
-auto future = qcor::submit([&](qcor::qpu_handler &qh) {
+  auto future = qcor::submit([&](qcor::qpu_handler &qh) {
     qh.vqe(
         [&](double x) {
           X(0);
@@ -27,6 +27,4 @@ auto future = qcor::submit([&](qcor::qpu_handler &qh) {
 
   auto results = future.get();
   auto energy = mpark::get<double>(results->getInformation("opt-val"));
-  std::cout << "Results: " << energy << "\n";
-  //   results->print();
 }
