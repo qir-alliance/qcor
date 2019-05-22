b2a9c80099f15465f6532f76199f1d397d1c7010
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-05-22 14:45:02 +0000

    updating formatting
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- examples/deuteron.cpp
+++ examples/deuteron.cpp
@@ -5,10 +5,12 @@ int main(int argc, char **argv) {
   qcor::Initialize(argc, argv);
 
   auto optimizer = qcor::getOptimizer(
-      "nlopt", {{"nlopt-optimizer", "cobyla"}, {"nlopt-maxeval", 20}});
+      "nlopt", {{"nlopt-optimizer", "cobyla"},
+                {"nlopt-maxeval", 20}});
 
-  auto op = qcor::getObservable(
-      "pauli", "5.907 - 2.1433 X0X1 - 2.1433 Y0Y1 + .21829 Z0 - 6.125 Z1");
+  auto op = qcor::getObservable("pauli", "5.907 - 2.1433 X0X1 "
+                                         "- 2.1433 Y0Y1"
+                                         "+ .21829 Z0 - 6.125 Z1");
 
   auto future = qcor::submit([&](qcor::qpu_handler &qh) {
     qh.vqe(
@@ -21,6 +23,4 @@ int main(int argc, char **argv) {
   });
 
   auto results = future.get();
-//   std::cout << "Results:\n";
-//   results->print();
 }
