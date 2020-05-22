9a88d316f8500c2e985d399531a95747d829e470
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-05-22 14:27:56 -0400

    updating with latest from xacc, c++17, new heterogeneous map impl
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- runtime/objectives/vqe/vqe.cpp
+++ runtime/objectives/vqe/vqe.cpp
@@ -7,24 +7,35 @@
 #include <memory>
 #include <set>
 
-using namespace cppmicroservices;
+#include "xacc.hpp"
+#include "xacc_internal_compiler.hpp"
 
+using namespace cppmicroservices;
 
 namespace qcor {
 
 class VQE : public ObjectiveFunction {
 protected:
+  std::shared_ptr<xacc::Algorithm> vqe;
   double operator()() override {
+    if (!vqe) {
+      vqe = xacc::getAlgorithm("vqe");
+    }
+    vqe->initialize(
+        {std::make_pair("ansatz", kernel),
+         std::make_pair("accelerator", xacc::internal_compiler::get_qpu()),
+         std::make_pair("observable", observable)});
+
     auto tmp_child = qalloc(qreg.size());
-    auto val = qcor::__internal__::observe(kernel, observable, tmp_child);
+    auto val = vqe->execute(xacc::as_shared_ptr(tmp_child.results()), {})[0];
     qreg.addChild(tmp_child);
     return val;
   }
+
 public:
   const std::string name() const override { return "vqe"; }
   const std::string description() const override { return ""; }
 };
-
 
 } // namespace qcor
 
