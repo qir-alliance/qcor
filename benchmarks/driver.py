13fe605284e538b21358ff608883854b6d2a3d07
Thien Nguyen <nguyentm@ornl.gov>
2020-08-04 16:37:56 +0000

    Implemented basic log parser to collect QCOR opt data
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- benchmarks/driver.py
+++ benchmarks/driver.py
@@ -4,16 +4,52 @@ import os
 import sys
 import subprocess
 import pathlib
+import csv
+import re
+
 qcorExe = str(pathlib.Path.home()) + "/.xacc/bin/qcor"
 
 # CSV result
 # Headers:
-resultCsv = "Test Case, Staq, Circuit Optimizer, Gate Merge, Level 1 Combined, \n"
+headers = ["Test Case", 
+          "Staq-Walltime", "Staq-Before", "Staq-After", 
+          "Gate Merge-Walltime", "Gate Merge-Before", "Gate Merge-After", 
+          "Circuit Optimizer-Walltime", "Circuit Optimizer-Before", "Circuit Optimizer-After", 
+          "XACC Built-in Combined-Walltime", "XACC Built-in Combined-Before", "XACC Built-in Combined-After", 
+          "Level 1 Combined-Walltime", "Level 1 Combined-Before", "Level 1 Combined-After"]
 
 # Extract data from log and append data to CSV
-def analyzeLog(log):
-  # TODO
-  pass
+firstWrite = True
+resultCsvFn = "result.csv"
+
+if os.path.exists(resultCsvFn):
+  os.remove(resultCsvFn)
+
+def analyzeLog(testCaseName, log):
+  global firstWrite
+  rowData = [os.path.splitext(testCaseName)[0]]
+  for line in log.splitlines():
+    # print("Receive '", line)
+    # Get elapsed time:
+    if "Elapsed time:" in line: 
+      splitResult = re.search("Elapsed time:(.*)\[ms\]", line)
+      rowData.append(float(splitResult.group(1)))
+    # Get before:
+    if "Number of Gates Before:" in line: 
+      splitResult = re.search("Number of Gates Before: (.*)", line)
+      rowData.append(int(splitResult.group(1)))
+    # Get after:
+    if "Number of Gates After:" in line: 
+      splitResult = re.search("Number of Gates After: (.*)", line)
+      rowData.append(int(splitResult.group(1)))
+  print("Data: ", rowData)
+  with open('result.csv', 'a', newline='') as csvfile:
+    resultWriter = csv.writer(csvfile)
+    if firstWrite is True:
+      resultWriter.writerow(headers)
+      firstWrite = False
+    # Write data
+    resultWriter.writerow(rowData)
 
 # Configurations:
 # Optimization level to run:
@@ -37,6 +73,6 @@ for srcFile in listOfSrcFiles:
   testExe = dirPath + "/" + testCaseName
   exeProcess = subprocess.Popen([testExe], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   stdout, stderr = exeProcess.communicate()
-  analyzeLog(stdout.decode("utf-8")) 
+  analyzeLog(testCaseName, stdout.decode("utf-8")) 
   os.remove(testExe)
   
