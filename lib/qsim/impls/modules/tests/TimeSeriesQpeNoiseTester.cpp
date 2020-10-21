1b471fc25839dd9ef25f700c930979e7b6227bd5
Thien Nguyen <nguyentm@ornl.gov>
2020-10-21 14:48:44 +0000

    Added base test script to test Ising model
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- lib/qsim/impls/modules/tests/TimeSeriesQpeNoiseTester.cpp
+++ lib/qsim/impls/modules/tests/TimeSeriesQpeNoiseTester.cpp
@@ -1,8 +1,15 @@
 #include "qcor.hpp"
 #include "qcor_qsim.hpp"
 #include "xacc.hpp"
+#include "xacc_service.hpp"
 #include <gtest/gtest.h>
 
+// These tests take quite some time, hence just run one at a time...
+//#define TEST_SIMPLE
+//#define TEST_DEUTERON
+#define TEST_ISING
+
+#ifdef TEST_SIMPLE
 TEST(TimeSeriesQpeNoiseTester, checkSimple) {
   const std::string NOISE_MODEL_JSON_FILE =
       std::string(RESOURCE_DIR) + "/ibmqx2_noise_model.json";
@@ -39,7 +46,9 @@ TEST(TimeSeriesQpeNoiseTester, checkSimple) {
     EXPECT_NEAR(expVal, theoreticalExp, 0.1);
   }
 }
+#endif
 
+#ifdef TEST_DEUTERON
 TEST(TimeSeriesQpeNoiseTester, checkDeuteron) {
   const std::string NOISE_MODEL_JSON_FILE =
       std::string(RESOURCE_DIR) + "/ibmqx2_noise_model.json";
@@ -74,6 +83,55 @@ TEST(TimeSeriesQpeNoiseTester, checkDeuteron) {
     std::cout << "Angle = " << angle << ": Exp val = " << expVal << "\n";
   }
 }
+#endif
+
+#ifdef TEST_ISING
+TEST(TimeSeriesQpeNoiseTester, checkIsingModel) {
+  const int nbShots = 1024 * 1024;
+  auto accelerator = xacc::getAccelerator("aer", {{"shots", nbShots}});
+  using namespace qcor;
+  const int nbQubits = 4;
+  const double Jz = 1.0;
+  const double Jx = 1.0;
+  xacc::quantum::PauliOperator hamOpZ, hamOpX;
+  for (int j = 0; j < nbQubits; ++j) {
+    hamOpZ += (Jz * Z(j));
+  }
+  for (int j = 0; j < nbQubits; ++j) {
+    // Periodic boundary condition (modulo nbQubits)
+    hamOpX += (Jx * X(j) * X((j + 1) % nbQubits));
+  }
+  // Total Hamiltonian
+  auto hamOp = hamOpZ + hamOpX;
+  std::cout << "Ham:\n" << hamOp.toString() << "\n";
+  // Run the QPE with verification.
+  auto evaluator = qsim::getObjEvaluator(hamOp, "qpe", {{"verified", true}});
+  auto provider = xacc::getIRProvider("quantum");
+  // Run the test with shots:
+  xacc::internal_compiler::qpu = accelerator;
+  // Variational parameters: 2 values for 1 layer:
+  const double pz = 0.123;
+  const double px = 0.567;
+  // Using only 1 layer (Eq. 73, p = 1)
+  auto kernel = provider->createComposite("test");
+  {
+    auto expZ = std::dynamic_pointer_cast<xacc::quantum::Circuit>(
+        xacc::getService<xacc::Instruction>("exp_i_theta"));
+    EXPECT_TRUE(expZ->expand({std::make_pair("pauli", hamOpZ.toString())}));
+    kernel->addInstruction(expZ->operator()({pz}));
+  }
+  {
+    auto expX = std::dynamic_pointer_cast<xacc::quantum::Circuit>(
+        xacc::getService<xacc::Instruction>("exp_i_theta"));
+    EXPECT_TRUE(expX->expand({std::make_pair("pauli", hamOpX.toString())}));
+    kernel->addInstruction(expX->operator()({px}));
+  }
+
+  // std::cout << "Kernel:\n" << kernel->toString() << "\n";
+  const auto expVal = evaluator->evaluate(kernel);
+  std::cout << "Exp-val:\n" << expVal << "\n";
+}
+#endif
 
 int main(int argc, char **argv) {
   xacc::Initialize();
