0cb1ce53579cc0c9148ee57ea93a10c2333639b2
Thien Nguyen <nguyentm@ornl.gov>
2020-12-18 16:07:59 +0000

    Port adapt to QSim
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- lib/qsim/impls/workflow/adapt.cpp
+++ lib/qsim/impls/workflow/adapt.cpp
@@ -1,5 +1,6 @@
-#include "qsim_utils.hpp"
 #include "adapt.hpp"
+#include "qsim_utils.hpp"
+#include "xacc.hpp"
 
 namespace qcor {
 namespace qsim {
@@ -13,12 +14,49 @@ bool AdaptVqeWorkflow::initialize(const HeterogeneousMap &params) {
     optimizer = createOptimizer(DEFAULT_OPTIMIZER);
   }
   config_params = params;
+
+  if (!params.stringExists("gradient_strategy")) {
+    config_params.insert("gradient_strategy", "autodiff");
+  }
+  
   return (optimizer != nullptr);
 }
 
 QuantumSimulationResult
 AdaptVqeWorkflow::execute(const QuantumSimulationModel &model) {
-  return {};
+  // Note: to make sure compatibility with XACC, we use the same
+  // parameters for Adapt as XACC, just forward it to the XACC impl.
+  
+  // Some extra adapt params comming from the model.
+  auto accelerator = xacc::internal_compiler::get_qpu();
+  xacc::HeterogeneousMap extra_params{{"observable", xacc::as_shared_ptr(model.observable)},
+                                      {"sub-algorithm", "vqe"},
+                                      {"accelerator", accelerator},
+                                      {"optimizer", optimizer}};
+
+  // If the model contains an ansatz:
+  if (model.user_defined_ansatz) {
+    std::shared_ptr<xacc::CompositeInstruction> state_prep_circ =
+        model.user_defined_ansatz->evaluate_kernel({});
+    extra_params.insert("initial-state", state_prep_circ);
+  }
+
+  auto adapt_params = config_params;
+  adapt_params.merge(extra_params);
+  auto adapt = xacc::getAlgorithm("adapt", adapt_params);
+  auto buffer = xacc::qalloc(model.observable->nBits());
+  adapt->execute(buffer);
+  auto opt_val = (*buffer)["opt-val"].as<double>();
+  auto opt_params = (*buffer)["opt-params"].as<std::vector<double>>();
+  auto opt_ansatz = (*buffer)["opt-ansatz"].as<std::vector<int>>();
+
+  return {
+      {"opt-val", opt_val},
+      // Alias opt-val to energy as well
+      {"energy", opt_val},
+      {"opt-params", opt_params},
+      {"opt-ansatz", opt_ansatz},
+  };
 }
 } // namespace qsim
 } // namespace qcor
