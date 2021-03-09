c236167f0698a2720b22cd157e518eb9b4b043f9
Thien Nguyen <nguyentm@ornl.gov>
2021-03-09 01:39:06 +0000

    Added qir checks
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- mlir/qir_qrt/tests/QirQrtTester.cpp
+++ mlir/qir_qrt/tests/QirQrtTester.cpp
@@ -1,9 +1,12 @@
 #include "gtest/gtest.h"
 #include <xacc.hpp>
 #include "qir-qrt.hpp"
+#include "qrt.hpp"
+#include "xacc_service.hpp"
 
 TEST(QirQrtTester, checkSimple) {
-  std::cout << "HOWDY\n";
+  ::quantum::qrt_impl = xacc::getService<::quantum::QuantumRuntime>("nisq");
+  ::quantum::qrt_impl->initialize("empty");
   int nbQubits = 5;
   auto arrayPtr = __quantum__rt__qubit_allocate_array(nbQubits);
   EXPECT_EQ(arrayPtr->size(), nbQubits);
@@ -21,6 +24,15 @@ TEST(QirQrtTester, checkSimple) {
         __quantum__rt__array_get_element_ptr_1d(arrayPtr, i + 1)));
 
     __quantum__qis__cnot(src, tgt);
+  }
+  std::cout << "HOWDY:\n"
+            << ::quantum::qrt_impl->get_current_program()->toString() << "\n";
+  EXPECT_EQ(::quantum::qrt_impl->get_current_program()->nInstructions(), 9);
+  for (int i = 0; i < nbQubits; ++i) {
+    auto inst = ::quantum::qrt_impl->get_current_program()->getInstruction(i);
+    EXPECT_EQ(inst->name(), "H");
+    EXPECT_EQ(inst->bits().size(), 1);
+    EXPECT_EQ(inst->bits()[0], i);
   }
 }
 
