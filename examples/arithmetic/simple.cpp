81d6ac323cbc11c75a0c26228353c58428c24adb
Thien Nguyen <nguyentm@ornl.gov>
2021-05-13 18:37:08 +0000

    Added adder test
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- examples/arithmetic/simple.cpp
+++ examples/arithmetic/simple.cpp
@@ -9,19 +9,22 @@ __qpu__ void test_adder(qreg a, qreg b, qubit cin, qubit cout) {
   Measure(cout);
 }
 
-// Compile:
 int main(int argc, char **argv) {
+  set_shots(1024);
   // Set the inputs to the adder
   auto a = qalloc(2);
   auto b = qalloc(2);
   auto carry_in = qalloc(1);
   auto carry_out = qalloc(1);
   // Execute:
-  // test_adder();
-  // test_adder::print_kernel(a, b, carry_in[0], carry_out[0]);
-  test_adder(a, b, carry_in[0], carry_out[0]);
+  test_adder::print_kernel(a, b, carry_in[0], carry_out[0]);
+  test_adder(b, a, carry_in[0], carry_out[0]);
   b.print(); // 00
-  carry_out.print(); // 1
+  qcor_expect(b.counts().size() == 1);
+  qcor_expect(b.counts()["00"] == 1024);
 
+  carry_out.print(); // 1
+  qcor_expect(carry_out.counts().size() == 1);
+  qcor_expect(carry_out.counts()["1"] == 1024);
   return 0;
 }
--- lib/impl/integer_arithmetic.hpp
+++ lib/impl/integer_arithmetic.hpp
@@ -5,17 +5,29 @@
 __qpu__ void majority(qubit a, qubit b, qubit c) {
   X::ctrl(c, b);
   X::ctrl(c, a);
-  X::ctrl({a, b}, c);
+  X::ctrl({b, a}, c);
+}
+
+// Note: this is not the adjoint of majority....
+// Could be a good use case for per-qubit uncompute...
+__qpu__ void unmajority(qubit a, qubit b, qubit c) {
+  X::ctrl({b, a}, c);
+  X::ctrl(c, a);
+  X::ctrl(a, b);
 }
 
 // Add a to b and save to b
 // c_in and c_out are carry bits (in and out)
 __qpu__ void ripple_add(qreg a, qreg b, qubit c_in, qubit c_out) {
-  compute {
-    majority(c_in, b[0], a[0]);
-    for (auto j : range(a.size() - 1)) {
-      majority(a[j], b[j + 1], a[j + 1]);
-    }
+  majority(c_in, b[0], a[0]);
+  for (auto j : range(a.size() - 1)) {
+    majority(a[j], b[j + 1], a[j + 1]);
   }
-  action { X::ctrl(a.tail(), c_out); }
+  
+  X::ctrl(a.tail(), c_out);
+
+  for (int j = a.size() - 2; j >= 0; --j) {
+    unmajority(a[j], b[j + 1], a[j + 1]);
+  }
+  unmajority(c_in, b[0], a[0]);
 }
