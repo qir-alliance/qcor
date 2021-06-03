1f1cb507247a7fdc4a98dc2583e8e49407b576b7
Thien Nguyen <nguyentm@ornl.gov>
2021-06-03 19:33:29 +0000

    Update the deuteron ftqc example to reduce execution time
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- examples/ftqc_qrt/deuteron.cpp
+++ examples/ftqc_qrt/deuteron.cpp
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
   // Hamiltonian
   auto H = 5.907 - 2.1433 * X(0) * X(1) - 2.1433 * Y(0) * Y(1) + .21829 * Z(0) -
            6.125 * Z(1);
-  auto optimizer = createOptimizer("nlopt");
+  auto optimizer = createOptimizer("nlopt", {{"maxeval", 20}, {"ftol", 0.1}});
   OptFunction f(
       [&](const std::vector<double> &x, std::vector<double> &dx) {
         const double angle = x[0];
