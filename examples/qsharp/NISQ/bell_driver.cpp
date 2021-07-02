376fbfbfae81f10dd76a6988e71be4730bbe434a
Thien Nguyen <nguyentm@ornl.gov>
2021-07-02 15:45:57 +0000

    Update Q# NISQ examples
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- examples/qsharp/NISQ/bell_driver.cpp
+++ examples/qsharp/NISQ/bell_driver.cpp
@@ -4,6 +4,9 @@
 
 // Util pre-processor to wrap Q# operation 
 // in a QCOR QuantumKernel.
+// Compile:
+// Note: need to use alpha package since this kernel will take a qubit array.
+// qcor -qdk-version 0.17.2106148041-alpha bell.qs bell_driver.cpp -shots 1024
 qcor_import_qsharp_kernel(QCOR__Bell);
 
 int main() {
