bd726ae21ab8293584890ca942768dcd97df638a
Thien Nguyen <nguyentm@ornl.gov>
2021-08-11 09:15:14 -0400

    Move csp benchmark to sub-folder and add pytket benchmark script
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

---
+++ benchmarks/openqasm3/trotter/pytket_script.py
@@ -0,0 +1,35 @@
+import time
+from pytket.circuit import Circuit, PauliExpBox
+from pytket.pauli import Pauli
+from pytket.extensions.qiskit import AerStateBackend
+from pytket.passes import FullPeepholeOptimise
+
+nb_steps = 100
+nb_qubits = 50
+step_size = 0.01
+# Start timer
+start = time.time()
+
+circ = Circuit(nb_qubits)
+
+h = 1.0
+Jz = 1.0
+for i in range(nb_steps):
+    # Using Heisenberg Hamiltonian:
+    for q in range(nb_qubits):
+        circ.add_pauliexpbox(PauliExpBox([Pauli.X], -h * step_size), [q])
+    for q in range(nb_qubits - 1):
+        circ.add_pauliexpbox(PauliExpBox([Pauli.Z, Pauli.Z], -Jz * step_size), [q, q + 1])
+
+# Compile to gates
+backend = AerStateBackend()
+circ = backend.get_compiled_circuit(circ)
+
+# Apply optimization
+FullPeepholeOptimise().apply(circ)
+
+end = time.time()
+print('Elapsed time =', end - start, '[secs]')
+
+for com in circ: # equivalently, circ.get_commands()
+  print(com.op, com.op.type, com.args)
